9b19f383fdfc48bd943e8248cbed636aa2112163
Raselj71 <raselj44@gmail.com>
2025-05-07 18:00:25 +0600

    added search  and fetching data functionlity

--- src/app/(dashbord)/property/[id]/page.tsx
+++ src/app/(dashbord)/property/[id]/page.tsx
@@ -1,6 +1,18 @@
+import { createClient } from '@/utils/supabase/ServerClient';
 import React from 'react'
 
-function page() {
+async function page(props: {
+	params: Promise<{ [key: string]: string | string[] }>;
+}) {
+
+   const param= await props.params
+   console.log(param)
+  const supabase = await createClient();
+  const {data, error}= await supabase.from('Property').select('*')
+
+  console.log(data)
+   
+
   return (
     <div>page</div>
   )
