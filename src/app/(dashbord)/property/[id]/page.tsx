d534bdeb37ff51d19871d671f9bc3cff1d31844b
Raselj71 <raselj44@gmail.com>
2025-05-11 01:43:33 +0600

    added search functionality

--- src/app/(dashbord)/property/[id]/page.tsx
+++ src/app/(dashbord)/property/[id]/page.tsx
@@ -1,10 +1,14 @@
+import DistrictItem from '@/components/common/DistrictItem'
 import PropertyList from '@/components/common/PropertyList'
+import UpzilaItem from '@/components/common/UpzilaItem'
+import { getDistrictsByDivision, getUpazilasByDistrict } from '@/utils/Address'
+
 import { createClient } from '@/utils/supabase/ServerClient'
-import { Box, Heading } from '@radix-ui/themes'
+import { Box, Flex, Heading, Separator } from '@radix-ui/themes'
 
 interface PageProps {
   params: Promise<{ id: string }>
-  searchParams?:Promise < { page?: string } >
+  searchParams?:Promise < { page?: string ,district?:string , upzila?:string} >
 }
 
 export default async function Page({ params, searchParams }: PageProps) {
@@ -18,15 +22,55 @@ export default async function Page({ params, searchParams }: PageProps) {
 
   const supabase = await createClient()
 
-  const { data, count } = await supabase
-    .from('Property')
-    .select('*', { count: 'exact' })
-    .eq('propertyDivision', paramsData.id)
-    .range(from, to)
+let query = supabase
+  .from('Property')
+  .select('*', { count: 'exact' })
+  .eq('propertyDivision', paramsData.id)
+
+if (searchParamsData?.district) {
+  query = query.eq('propertyDistrict', searchParamsData.district)
+}
+if(searchParamsData?.upzila){
+ query = query.eq('propertyUpzila', searchParamsData.upzila)
+}
+
+const { data, count } = await query.range(from, to)
+   let upzila:string[]=[]
+    const district=getDistrictsByDivision(paramsData.id)
+     if(searchParamsData?.district){
+          upzila=getUpazilasByDistrict(searchParamsData?.district)
+     }
+    
 
   return (
     <Box className=''>
-       <Heading className='first-letter:uppercase' mb={'3'}>{paramsData.id}</Heading>
+       <Box>
+          <Flex className={`${searchParamsData?.district? 'hidden':''}`} wrap={'wrap'} justify={'center'} gap={{
+            initial:'2',
+            lg:'4'
+          }}>
+               {
+                district.map((item)=>{
+                  return <DistrictItem key={item} item={item}/>
+                })
+               }
+          </Flex>
+          <Separator className='my-2' size={'4'}/>
+
+           <Flex wrap={'wrap'} justify={'center'} gap={{
+            initial:'2',
+            lg:'4'
+          }}>
+               {
+                upzila.map((item)=>{
+                  return <UpzilaItem key={item} item={item}/>
+                })
+               }
+          </Flex>
+
+           <Separator className='my-2' size={'4'}/>
+       </Box>
+       <Heading className='first-letter:uppercase' my={'3'}>{paramsData.id}</Heading>
       <PropertyList data={data || []} total={count || 0} page={page} perPage={perPage} />
     </Box>
   )
---
+++ src/components/common/DistrictItem.tsx
@@ -0,0 +1,25 @@
+'use client'
+import { Button } from '@radix-ui/themes'
+import { useRouter, useSearchParams } from 'next/navigation';
+import React from 'react'
+
+function DistrictItem({item}:{item:string}) {
+
+     const router = useRouter();
+      const searchParams = useSearchParams();
+      const selectedDistrict=searchParams.get('district')
+      const isSelected=selectedDistrict===item
+      
+  const hanldleClick=()=>{
+    const params = new URLSearchParams(searchParams.toString());
+    
+    params.set("district", item.toString());
+    router.push(`?${params.toString()}`);
+  }
+
+  return (
+     <Button className={`${isSelected? 'border-2! border-accent-10!':''}`} onClick={hanldleClick} variant='soft'>{item}</Button>
+  )
+}
+
+export default DistrictItem
